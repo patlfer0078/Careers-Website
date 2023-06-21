d55c4b10fb22d8eb9c47f5926e221447412e3d73
patlfer0078 <61334374+patlfer0078@users.noreply.github.com>
2023-06-21 19:25:19 +0000

    Added HTML and CSS w/ Bootstrap

--- main.py
+++ main.py
@@ -1,10 +1,40 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id':1, 
+    'title': 'Data Analyst',
+    'location': 'Dallas, Texas',
+    'salary': '63,000.00',
+  },
+  {
+    'id':2, 
+    'title': 'Data Scientist',
+    'location': 'San Antonio, Texas',
+    'salary': '80,000.00',
+  },
+  {
+    'id':3, 
+    'title': 'Frontend Engineer',
+    'location': 'Remote',
+    'salary': '56,000.00',
+  },
+  {
+    'id':4, 
+    'title': 'Backend Engineer',
+    'location': 'San Francisco, California',
+    'salary': '65,000.00',
+  }
+]
 @app.route("/")
 def hello_world():
-  return "Hello, World!"
+  return render_template('home.html', jobs=JOBS)
+
+@app.route("/api/jobs")
+def list_jobs():
+    return jsonify(JOBS)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
